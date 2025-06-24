c39606151b8d9974e107703631c2d6cf7bde3c30
octavius <octaviohernandez820@gmail.com>
2025-06-24 14:54:03 -0600

    flor-enna

--- obj/Debug/net9.0/MiMangaBot.AssemblyInfo.cs
+++ obj/Debug/net9.0/MiMangaBot.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiMangaBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07d29f604a8009b4c72a28ba2be94db8d7181f15")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a4f5859a50708b73ae4dd6fd20f73cbd875d4ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiMangaBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MiMangaBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
